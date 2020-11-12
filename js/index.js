01a82c0bb6ee314f9f749ca740dfc1a72e03f27d
Vladlen <vladvladx25@gmail.com>
2020-11-12 16:38:33 +0300

    first

---
+++ js/index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+// 1 Задание
+
+let a = 5, b = 6, h = 7;
+let S = 2*(a*b+b*h+a*h);
+console.log(S);
+
+// 2 Задание
+
+let n = 34; 
+let n1 = n % 10;
+let n2 = (n - n1) / 10;
+let sum = n1 + n2;
+let info = `Значение n равно ${n}, сумма его цифр ${sum}`;
+console.log(info);
+
+// 3 Задание
+
+let a1 = 15;
+let b1 = 31;
+
+a1 = a1 + b1;
+b1 = a1 - b1;
+a1 = a1 - b1;
+
+console.log(a1, b1);
+
+
